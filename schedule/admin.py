15ef37e46e25474320f5f760dd5ef8bc66b991dd
racoonxxi <choi235711@gmail.com>
2019-07-12 18:00:03 +0900

    schedule/admin.py

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    fields = ['name', 'schedule_of_teacher', 'schedule_of_group']
+    admin.site.register(Subject, SubjectAdmin)
+
+
+class ScheduleOfTeacher(admin.ModelAdmin):
+    fields = ['time', 'day_of_week', 'week', 'date']
+    admin.site.register(ScheduleOfTeacher, ScheduleOfTeacherAdmin)
+
+
+class ScheduleOfGroup(admin.ModelAdmin):
+    fields = ['time', 'day_of_week', 'week', 'date']
+    admin.site.register(ScheduleOfGroup, ScheduleOfGroupAdmin)
