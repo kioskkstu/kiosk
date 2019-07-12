12405247c58ec075e4416f9dc1b0cb61bd08c209
rioirinko <lina.rio@mail.ru>
2019-07-12 15:50:50 +0600

    change postgresql to mysql

--- kstu/settings.py
+++ kstu/settings.py
@@ -80,11 +80,9 @@ WSGI_APPLICATION = 'kstu.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': config("DB_ENGINE"),
-        'NAME': config("DB_NAME"),
-        'USER': config("DB_USER"),
-        'PASSWORD': config("DB_PASSWORD"),
-        'HOST': config("DB_HOST"),
-        'PORT': config("DB_PORT"),
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        }
     }
 }
 
