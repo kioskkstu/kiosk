e6cb68308366caada2ce48b2609a173a50b9ec4b
ElshopNeobis <electronics.shop4.neobis@gmail.com>
2019-07-16 15:03:48 +0600

    change some models and university admin

--- university/admin.py
+++ university/admin.py
@@ -2,9 +2,23 @@ from django.contrib import admin
 from .models import *
 
 
+class DepartmentInline(admin.StackedInline):
+    model = Department
+    extra = 1
+    fields = ('name', 'about')
+
+
+class FacultyAdmin(admin.ModelAdmin):
+    inlines = [DepartmentInline, ]
+
+
+class InstituteAdmin(admin.ModelAdmin):
+    inlines = [DepartmentInline, ]
+
+
 admin.site.register(PreUniversity)
-admin.site.register(Faculty)
-admin.site.register(Institute)
+admin.site.register(Faculty, FacultyAdmin)
+admin.site.register(Institute, InstituteAdmin)
 admin.site.register(Department)
 admin.site.register(Teacher)
 admin.site.register(Building)
