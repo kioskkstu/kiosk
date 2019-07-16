a62b3b1ffab0e47828307cc44ae14a022c385442
racoonxxi <choi235711@gmail.com>
2019-07-16 18:14:52 +0900

    schedule/admin.py

--- history/tests.py
+++ history/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import *
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+
+class HistoryModelTests(TestCase):
+    @classmethod
+    def setUp(self):
+        self.old_cnt = History.objects.count()
+        self.history = History.objects.create(name='test', text='test')
+
+    def test_model_create_History(self):
+        self.new_cnt = History.objects.count()
+        self.assertNotEqual(self.old_cnt, self.new_cnt)
+
+    def test_api_get_History(self):
+        response = self.client.get(
+            reverse('history'),
+            kwarge={'pk': self.history.id}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, self.history)
+
+    def test_api_get_History_detail(self):
+        response = self.client.get('/history/%s/' % self.history.id)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            (response.data['name'], response.data['text']),
+            ('test', 'test')
+        )
--- schedule/tests.py
+++ schedule/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from .models import *
+from university.models import *
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+
+class ScheduleModelTests(TestCase):
+    @classmethod
+    def setUp(self):
+        self.old_cnt_subject = Subject.objects.count()
+        self.old_cnt_schedule = Schedule.objects.count()
+
+        self.department = Department.objects.create(name='test', about='test')
+        self.building = Building.objects.create(name='1', location='1', floor='1')
+
+        self.subject = Subject.objects.create(name='test')
+
+        self.teacher = Teacher.objects.create(name='test', photo='', status='test', contact='1',
+                                              department=self.department)
+        self.classroom = Classroom.objects.create(name='test', floor='1', about='test',
+                                                  building=self.building)
+        self.group = Group.objects.create(name='test', department=self.department)
+
+        self.schedule = Schedule.objects.create(time='1', day_of_week='1', week='1', type='1', teacher=self.teacher,
+                                                classroom=self.classroom, subject=self.subject,
+                                                group=self.group)
+
+    def test_model_create_Subject(self):
+        self.new_cnt = Subject.objects.count()
+        self.assertNotEqual(self.old_cnt_subject, self.new_cnt)
+
+    def test_model_create_Schedule(self):
+        self.new_cnt = Schedule.objects.count()
+        self.assertNotEqual(self.old_cnt_schedule, self.new_cnt)
+
+    def test_api_get_ScheduleOFTeacher(self):
+        response = self.client.get(
+            reverse('teacher'),
+            kwargs={'teacher': self.teacher.id}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, self.teacher)
+
+    def test_api_get_ScheduleOFGroup(self):
+        response = self.client.get(
+            reverse('group'),
+            kwargs={'group': self.group.id}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, self.group)
